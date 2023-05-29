8443ce39c5cf2860afa1182c27e5067840d8248c
PauloGN <paulorsantos.gnt@outlook.com>
2023-05-28 18:35:27 -0700

    UE5.2 Sessions

---
+++ Source/Multiplayer/MyNetWorkComponent.h
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+//OnlineSessions
+#include "Interfaces/OnlineSessionInterface.h"
+//OnlineSessions End
+#include "MyNetWorkComponent.generated.h"
+
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class MULTIPLAYER_API UMyNetWorkComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UMyNetWorkComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable)
+	void OpenLobby();
+
+	UFUNCTION(BlueprintCallable)
+	void CallOpenLevel(const FString& address);
+
+	UFUNCTION(BlueprintCallable)
+	void CallOpenLevelClienteTravel(const FString& address);
+
+
+#pragma region Testing Online Sessions 
+
+public:
+
+	class IOnlineSubsystem* onlineSubsystem;
+	IOnlineSessionPtr onlineSessionInterface;
+	TSharedPtr<class FOnlineSessionSettings> sessionSettings; //https://docs.unrealengine.com/4.27/en-US/API/Plugins/OnlineSubsystem/FOnlineSessionSettings/
+	TSharedPtr<class FOnlineSessionSearch> sessionSearch; //https://docs.unrealengine.com/4.27/en-US/API/Plugins/OnlineSubsystem/FOnlineSession/
+
+
+protected:
+
+	//Calling from the editor UE5 via interface or input
+	UFUNCTION(BlueprintCallable)
+	void CreateGameSession();
+	
+	UFUNCTION(BlueprintCallable)
+	void JoinGameSession();
+
+	//Callback function
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnFindSessionComplete(bool bWasSuccessful);
+	void OnjoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+
+private:
+
+	/// <summary>
+	/// This delegate will be included to the sessionInterface delegate list
+	/// then this needs one or more callback functions to reponde when the delegate is fired
+	/// note: Check the signature for call back functions
+	/// note 2:  Initialize Delegate and bind callback function in the initialization list of the constructor
+	/// </summary>
+	FOnCreateSessionCompleteDelegate createSessionCompleteDelegate;
+	FOnFindSessionsCompleteDelegate findSessionsCompleteDelegate;
+	FOnJoinSessionCompleteDelegate joinSessionCompleteDelegate;
+
+#pragma endregion
+
+};
